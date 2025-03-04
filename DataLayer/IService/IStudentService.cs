184de2d06b49bf01f08ea9117d9efda5fad90255
Maksymilian Moszyński <mmoszynski@griddynamics.com>
2025-03-04 13:44:22 +0100

    designing DAL with console App toand 3 tier architecture to do module 3.6 and 4.1 at the same time

--- DataLayer/IService/IStudentService.cs
+++ DataLayer/IService/IStudentService.cs
@@ -1,7 +1,15 @@
-﻿namespace DataLayer.IService
+﻿using DataLayer.Models;
+using DataLayer.Models.ValueObject;
+
+namespace DataLayer.IService
 {
     public interface IStudentService
     {
-
+        Task<Student?> GetStudentInfo(int studentId);
+        Task<IEnumerable<ModuleMark>> GetModuleMarks(int studentId);
+        Task CreateLearningPlan(LearningPlan learningPlan, Student student);
+        Task<IEnumerable<Student>> GetAllStudentsWithLearningPlans(int studentId);
+        Task<IEnumerable<StudentGrade>> GetStudentsWithTheirAverageGrade();
+        Task UpdateStudentLearningPlan(LearningPlan learningPlan, Student student);
     }
 }
